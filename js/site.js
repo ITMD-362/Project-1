7cc1842152660c9db5589e25a53a0da44b251f86
akshaypatel <31392905+akshay1995p@users.noreply.github.com>
2018-03-13 18:58:50 -0500

    fixing the .js file and  and validating the code coreectly

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+
 (function () {
 
 function validateForm() {
@@ -28,3 +29,9 @@ document.forms["signup-form"].addEventListener('submit', validateForm);
 })();
 
 
+//When user clicks the photos it will take the user to the website associated wiht the photo
+$(document).ready(function(){
+$("#signup-form").click(function(){
+window.location.href = "https://www.bodybuilding.com/";
+});
+});
